c041f1f1a83a324b718a650d934ddfc27273f9a7
Filipe PS <filipe.panni@gmail.com>
2020-02-18 18:41:54 -0300

    adicionado suporte a mobile

--- background.js
+++ background.js
@@ -2,6 +2,27 @@ if (typeof browser !== 'undefined') {
     chrome = browser
 }
 
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
 var scriptInjectTranslateObj = null
 var scriptTranslateObj = null
 
@@ -51,4 +72,23 @@ chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
             unregisterScripts()
         }
     }
+
+    if (request.command == "translate") {
+        chrome.tabs.query({ currentWindow: true, active: true}, tabs => {
+            chrome.tabs.executeScript(tabs[0].id, { file: "/scripts/injectTranslate.js" })
+            chrome.tabs.executeScript(tabs[0].id, { file: "/scripts/translate.js" })
+        })
+    } else if (request.command == "showOriginal") {
+        chrome.tabs.query({ currentWindow: true, active: true}, tabs => {
+            chrome.tabs.executeScript(tabs[0].id, { file: "/scripts/restore.js" })
+        })
+    }
 })
+
+// if (isMobile.any()) {
+    chrome.contentScripts.register({
+        "js": [{file: "/scripts/mobile.js"}],
+        "matches": ["<all_urls>"],
+        "runAt": "document_end"
+    })
+// }
---
+++ scripts/mobile.js
@@ -0,0 +1,66 @@
+var eHtml = document.getElementsByTagName("html")[0]
+
+if (eHtml) {
+    var pageLang = eHtml.getAttribute("lang") || eHtml.getAttribute("xml:lang");
+}
+
+if (pageLang) {
+    pageLang = pageLang.split("-")[0]
+    var navigatorLang = navigator.language.split("-")[0]
+
+    if (navigatorLang != pageLang) {
+        injectPopup()
+    }
+}
+
+function injectPopup()
+{
+    var element = document.createElement("div")
+    element.setAttribute("translate", "no")
+    element.classList.add("notranslate")
+    element.style = `
+        position: fixed;
+        bottom: 0;
+        background-color: white;
+        box-shadow: 0px -1px 2px rgba(0, 0, 0, 0.6);
+        width: 100%;
+        height: 40px;
+        font-family: "Verdana";
+        font-size: 25px;
+    `
+    element.innerHTML = `
+    <div style="display:flex; align-items: center; margin: 0 auto;">
+    <button id="twpm-btnTranslate" style="flex: 1; text-align: center;">Translate</button>
+    <button id="twpm-btnShowOriginal" style="flex: 1; text-align: center;">Show Original</button>
+    <button id="twpm-btnClose" style="flex: 1; text-align: center; max-width: 40px;" id="twpm-btnClose">&times;</button>
+    </div>
+    `
+    document.body.appendChild(element)
+
+    var twpm_btnCloseIsClicked = false
+
+    window.addEventListener("scroll", () => {
+        if (window.pageYOffset < 10 && !twpm_btnCloseIsClicked) {
+            element.style.display = "block"
+        } else {
+            element.style.display = "none"
+        }
+    })
+
+    const twpm_btnTranslate= document.getElementById("twpm-btnTranslate")
+    const twpm_btnShowOriginal = document.getElementById("twpm-btnShowOriginal")
+    const twpm_btnClose = document.getElementById("twpm-btnClose")
+
+    twpm_btnTranslate.addEventListener("click", () => {
+        chrome.runtime.sendMessage({command: "translate"})
+    })
+
+    twpm_btnShowOriginal.addEventListener("click", () => {
+        chrome.runtime.sendMessage({command: "showOriginal"})
+    })
+
+    twpm_btnClose.addEventListener("click", () => {
+        twpm_btnCloseIsClicked = true
+        element.style.display = "none"
+    })
+}
