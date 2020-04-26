b9593bf7c037e08ecdaaf01b30fa9fbf32247930
Filipe PS <filipe.panni@gmail.com>
2020-04-26 20:26:23 -0300

    Adicionado opção de remover linguagens de auto tradução nas configurações

--- options/options.js
+++ options/options.js
@@ -4,6 +4,7 @@ if (typeof browser !== 'undefined') {
 
 const lblNeverTranslate = document.getElementById("lblNeverTranslate")
 const neverTranslateList = document.getElementById("neverTranslateList")
+const alwaysTranslateList = document.getElementById("alwaysTranslateList")
 
 lblNeverTranslate.textContent = chrome.i18n.getMessage("optionsNeverTranslate")
 
@@ -31,11 +32,25 @@ function removeSiteFromBlackList(url)
     })   
 }
 
+function disableAutoTranslate(lang)
+{
+    chrome.storage.local.get("alwaysTranslateLangs").then(onGot => {
+        var alwaysTranslateLangs = onGot.alwaysTranslateLangs
+        if (!alwaysTranslateLangs) {
+            alwaysTranslateLangs = []
+        }
+
+        removeA(alwaysTranslateLangs, lang)
+        chrome.storage.local.set({alwaysTranslateLangs})
+    })   
+}
+
 chrome.storage.local.get("neverTranslateSites").then(onGot => {
     var neverTranslateSites = onGot.neverTranslateSites
     if (!neverTranslateSites) {
         neverTranslateSites = []
     }
+    neverTranslateSites.sort()
 
     neverTranslateSites.forEach(value => {
         let li = document.createElement("li")
@@ -54,5 +69,39 @@ chrome.storage.local.get("neverTranslateSites").then(onGot => {
         li.appendChild(close)
         
         neverTranslateList.appendChild(li)
+    })
+})
+
+chrome.storage.local.get("alwaysTranslateLangs").then(async onGot => {
+    var alwaysTranslateLangs = onGot.alwaysTranslateLangs
+    if (!alwaysTranslateLangs) {
+        alwaysTranslateLangs = []
+    }
+    alwaysTranslateLangs.sort()
+
+    var interfaceLanguages = await chrome.i18n.getAcceptLanguages()
+
+    alwaysTranslateLangs.forEach(value => {
+        var language = codeToLanguage(value, interfaceLanguages[0])
+        if (!language) {
+            language = "\"" + value + "\""
+        }
+
+        let li = document.createElement("li")
+        li.setAttribute("class", "w3-display-container")
+        li.innerText = language
+
+        let close = document.createElement("span")
+        close.setAttribute("class", "w3-button w3-transparent w3-display-right")
+        close.innerHTML = "&times;"
+
+        close.addEventListener("click", () => {
+            li.style.display = "none"
+            disableAutoTranslate(value)
+        })
+
+        li.appendChild(close)
+        
+        alwaysTranslateList.appendChild(li)
     })
 })
