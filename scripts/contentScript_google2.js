6a0817bec9d4d1d4a1bbf906d52ef0bf18926480
Filipe <filipe.panni@gmail.com>
2020-07-04 11:34:27 -0300

    Melhorado o script de tradução do google

--- scripts/contentScript_google2.js
+++ scripts/contentScript_google2.js
@@ -9,73 +9,31 @@ var status = "prompt"
 var htmlTagsInlineText = ['#text', 'A', 'ABBR', 'B', 'BIG', 'BDO', 'B', 'CITE', 'DFN', 'EM', 'I', 'INST', 'KBD', 'TT', 'Q', 'SAMP', 'SMALL', 'SPAN', 'STRONG', 'SUB', 'SUP']
 var htmlTagsNoTranslate = ['CODE', 'TITLE', 'SCRIPT', 'STYLE', 'TEXTAREA']
 
-function shiftLeftOrRightThenSumOrXor(num, opArray) {
-    return opArray.reduce((acc, opString) => {
-        var op1 = opString[1]; //	'+' | '-' ~ SUM | XOR
-        var op2 = opString[0]; //	'+' | '^' ~ SLL | SRL
-        var xd = opString[2]; //	[0-9a-f]
-
-        var shiftAmount = hexCharAsNumber(xd);
-        var mask = (op1 == '+') ? acc >>> shiftAmount : acc << shiftAmount;
-        return (op2 == '+') ? (acc + mask & 0xffffffff) : (acc ^ mask);
-    }, num);
-}
-
-function hexCharAsNumber(xd) {
-    return (xd >= 'a') ? xd.charCodeAt(0) - 87 : Number(xd);
-}
-
-function transformQuery(query) {
-    for (var e = [], f = 0, g = 0; g < query.length; g++) {
-        var l = query.charCodeAt(g);
-        if (l < 128) {
-            e[f++] = l; //	0{l[6-0]}
-        } else if (l < 2048) {
-            e[f++] = l >> 6 | 0xC0; //	110{l[10-6]}
-            e[f++] = l & 0x3F | 0x80; //	10{l[5-0]}
-        } else if (0xD800 == (l & 0xFC00) && g + 1 < query.length && 0xDC00 == (query.charCodeAt(g + 1) & 0xFC00)) {
-            //	that's pretty rare... (avoid ovf?)
-            l = (1 << 16) + ((l & 0x03FF) << 10) + (query.charCodeAt(++g) & 0x03FF);
-            e[f++] = l >> 18 | 0xF0; //	111100{l[9-8*]}
-            e[f++] = l >> 12 & 0x3F | 0x80; //	10{l[7*-2]}
-            e[f++] = l & 0x3F | 0x80; //	10{(l+1)[5-0]}
-        } else {
-            e[f++] = l >> 12 | 0xE0; //	1110{l[15-12]}
-            e[f++] = l >> 6 & 0x3F | 0x80; //	10{l[11-6]}
-            e[f++] = l & 0x3F | 0x80; //	10{l[5-0]}
-        }
+function Cp(a, b) {
+    for (var c = 0; c < b.length - 2; c += 3) {
+        var d = b.charAt(c + 2);
+        d = 'a' <= d ? d.charCodeAt(0) - 87 : Number(d);
+        d = '+' == b.charAt(c + 1) ? a >>> d : a << d;
+        a = '+' == b.charAt(c) ? a + d & 4294967295 : a ^ d
     }
-    return e;
+    return a
 }
 
-function normalizeHash(encondindRound2) {
-    if (encondindRound2 < 0) {
-        encondindRound2 = (encondindRound2 & 0x7fffffff) + 0x80000000;
+function calcHash(a, b) {
+    var c = b.split('.');
+    b = Number(c[0]) || 0;
+    for (var d = [], e = 0, f = 0; f < a.length; f++) {
+        var h = a.charCodeAt(f);
+        128 > h ? d[e++] = h : (2048 > h ? d[e++] = h >> 6 | 192 : (55296 == (h & 64512) && f + 1 < a.length && 56320 == (a.charCodeAt(f + 1) & 64512) ? (h = 65536 + ((h & 1023) << 10) + (a.charCodeAt(++f) & 1023), d[e++] = h >> 18 | 240, d[e++] = h >> 12 & 63 | 128) : d[e++] = h >> 12 | 224, d[e++] = h >> 6 & 63 | 128), d[e++] = h & 63 | 128)
     }
-    return encondindRound2 % 1E6;
-}
-
-function calcHash(query, windowTkk) {
-    //	STEP 1: spread the the query char codes on a byte-array, 1-3 bytes per char
-    var bytesArray = transformQuery(query);
-
-    //	STEP 2: starting with TKK index, add the array from last step one-by-one, and do 2 rounds of shift+add/xor
-    var d = windowTkk.split('.');
-    var tkkIndex = Number(d[0]) || 0;
-    var tkkKey = Number(d[1]) || 0;
-
-    var encondingRound1 = bytesArray.reduce((acc, current) => {
-        acc += current;
-        return shiftLeftOrRightThenSumOrXor(acc, ['+-a', '^+6'])
-    }, tkkIndex);
-
-    //	STEP 3: apply 3 rounds of shift+add/xor and XOR with they TKK key
-    var encondingRound2 = shiftLeftOrRightThenSumOrXor(encondingRound1, ['+-3', '^+b', '+-f']) ^ tkkKey;
-
-    //	STEP 4: Normalize to 2s complement & format
-    var normalizedResult = normalizeHash(encondingRound2);
-
-    return normalizedResult.toString() + "." + (normalizedResult ^ tkkIndex)
+    a = b;
+    for (e = 0; e < d.length; e++) a += d[e],
+        a = Cp(a, '+-a^+6');
+    a = Cp(a, '+-3^+b+-f');
+    a ^= Number(c[1]) || 0;
+    0 > a && (a = (a & 2147483647) + 2147483648);
+    c = a % 1000000;
+    return c.toString() + '.' + (c ^ b)
 }
 
 var googleTranslateTKK = null
