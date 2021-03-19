880ecfb35e05731e591159ae3eba9ba0f3ced514
Filipe PS <filipe.panni@gmail.com>
2021-03-19 15:36:07 -0300

    Adicionado botões para alterar idioma de destino e serviçõ de tradução no popup do texto selecionado

--- contentScript/translateSelected.js
+++ contentScript/translateSelected.js
@@ -6,10 +6,12 @@ var translateSelected = {}
 
 twpConfig.onReady(function() {
     let divElement
-    let selTextButton
-    let selTextTranslated
+    let eButtonTransSelText
+    let eDivResult
+    let eSelTextTrans
 
     let originalPageLanguage = "und"
+    let currentTargetLanguages = twpConfig.get("targetLanguages")
     let currentTargetLanguage = twpConfig.get("targetLanguages")[0]
     let currentPageTranslatorService = twpConfig.get("pageTranslatorService")
     let translateThisSite = twpConfig.get("neverTranslateSites").indexOf(location.hostname) === -1
@@ -55,33 +57,37 @@ twpConfig.onReady(function() {
         const shadowRoot = divElement.attachShadow({mode: "closed"})
         shadowRoot.innerHTML = `
             <style>
-                #selTextTranslated {
+                #eDivResult {
                     all: initial;
                     font-family: 'Helvetica', 'Arial', sans-serif;
                     font-style: normal;
                     font-variant: normal;
                     line-height: normal;
-                    font-size: 14px;
+                    font-size: 12px;
                     font-weight: 500;
 
                     z-index: 2147483647;
                     position: fixed;
                     border-radius: 5px;
-                    max-width: 280px;
-                    max-height: 250px;
                     top: 0px;
                     left: 0px;
                     background-color: white;
                     color: black;
                     border: 1px solid grey;
-                    overflow: auto;
-                    padding: 16px;
                     visible: true;
                     opacity: 1;
                     display: none;
                 }
 
-                #selTextButton {
+                #eSelTextTrans {
+                    padding: 14px;
+                    font-size: 16px;
+                    max-width: 360px;
+                    max-height: 260px;
+                    overflow: auto;
+                }
+
+                #eButtonTransSelText {
                     all: initial;
                     z-index: 2147483647;
                     position: fixed;
@@ -97,9 +103,101 @@ twpConfig.onReady(function() {
                     opacity: 1;
                     display: none;
                 }
+
+                ul {
+                    margin: 0px;
+                    padding: 3px;
+                    list-style-type: none;
+                    overflow: hidden;
+                }
+
+                li {
+                    margin: 3px;
+                    float: left;
+                    cursor: pointer;
+                    text-align: center;
+                    padding: 4px 6px;
+                    border-radius: 6px;
+                    box-shadow: 0 0 2px 0px #999;
+                }
+
+                li:hover {
+                    background-color: #ccc;
+                }
+
+                hr {
+                    border: 1px solid #ddd;
+                }
+
+                .selected {
+                    background-color: #ccc;
+                }
             </style>
-            <div id="selTextButton"></div>
-            <div id="selTextTranslated"></div>
+            <div id="eButtonTransSelText"></div>
+            <div id="eDivResult">
+                <div id="eSelTextTrans"></div>
+                <hr>
+                <div style="display: flex; justify-content: space-between; flex-direction:row;">
+                    <ul id="setTargetLanguage">
+                        <li value="en" title="English">en</li>
+                        <li value="es" title="Spanish">es</li>
+                        <li value="de" title="German">de</li>
+                    </ul>
+                    <ul>
+                        <li title="Google" id="sGoogle">g</li>
+                        <li title="Yandex" id="sYandex">y</li>
+                        <li style="padding-top: 6px; padding-bottom: 2px;" title="Listen" data-i18n-title="btnListen" id="listen">
+                            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                            width="14px" height="12px" viewBox="0 0 93.038 93.038" style="enable-background:new 0 0 93.038 93.038;"
+                            xml:space="preserve">
+                            <g>
+                                <path d="M46.547,75.521c0,1.639-0.947,3.128-2.429,3.823c-0.573,0.271-1.187,0.402-1.797,0.402c-0.966,0-1.923-0.332-2.696-0.973
+                                    l-23.098-19.14H4.225C1.892,59.635,0,57.742,0,55.409V38.576c0-2.334,1.892-4.226,4.225-4.226h12.303l23.098-19.14
+                                    c1.262-1.046,3.012-1.269,4.493-0.569c1.481,0.695,2.429,2.185,2.429,3.823L46.547,75.521L46.547,75.521z M62.784,68.919
+                                    c-0.103,0.007-0.202,0.011-0.304,0.011c-1.116,0-2.192-0.441-2.987-1.237l-0.565-0.567c-1.482-1.479-1.656-3.822-0.408-5.504
+                                    c3.164-4.266,4.834-9.323,4.834-14.628c0-5.706-1.896-11.058-5.484-15.478c-1.366-1.68-1.24-4.12,0.291-5.65l0.564-0.565
+                                    c0.844-0.844,1.975-1.304,3.199-1.231c1.192,0.06,2.305,0.621,3.061,1.545c4.977,6.09,7.606,13.484,7.606,21.38
+                                    c0,7.354-2.325,14.354-6.725,20.24C65.131,68.216,64.007,68.832,62.784,68.919z M80.252,81.976
+                                    c-0.764,0.903-1.869,1.445-3.052,1.495c-0.058,0.002-0.117,0.004-0.177,0.004c-1.119,0-2.193-0.442-2.988-1.237l-0.555-0.555
+                                    c-1.551-1.55-1.656-4.029-0.246-5.707c6.814-8.104,10.568-18.396,10.568-28.982c0-11.011-4.019-21.611-11.314-29.847
+                                    c-1.479-1.672-1.404-4.203,0.17-5.783l0.554-0.555c0.822-0.826,1.89-1.281,3.115-1.242c1.163,0.033,2.263,0.547,3.036,1.417
+                                    c8.818,9.928,13.675,22.718,13.675,36.01C93.04,59.783,88.499,72.207,80.252,81.976z"/>
+                            </g>
+                            <g>
+                            </g>
+                            <g>
+                            </g>
+                            <g>
+                            </g>
+                            <g>
+                            </g>
+                            <g>
+                            </g>
+                            <g>
+                            </g>
+                            <g>
+                            </g>
+                            <g>
+                            </g>
+                            <g>
+                            </g>
+                            <g>
+                            </g>
+                            <g>
+                            </g>
+                            <g>
+                            </g>
+                            <g>
+                            </g>
+                            <g>
+                            </g>
+                            <g>
+                            </g>
+                            </svg>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         `
 
         function enableDarkMode() {
@@ -111,18 +209,30 @@ twpConfig.onReady(function() {
                     * {
                         scrollbar-color: #202324 #454a4d;
                     }
-                    #selTextTranslated {
+                    #eDivResult {
                         color: rgb(231, 230, 228) !important;
                         background-color: #181a1b !important;
                     }
+                    hr {
+                        border-color: #666;
+                    }
+                    li:hover {
+                        color: rgb(231, 230, 228) !important;
+                        background-color: #454a4d !important;
+                    }
+                    .selected {
+                        background-color: #454a4d !important;
+                    }
                 `
                 shadowRoot.appendChild(el)
+                shadowRoot.querySelector("#listen svg").style = "fill: rgb(231, 230, 228)"
             }
         }
         
         function disableDarkMode() {
             if (shadowRoot.getElementById("#darkModeElement")) {
                 shadowRoot.getElementById("#darkModeElement").remove()
+                shadowRoot.querySelector("#listen svg").style = "fill: black"
             }
         }
         
@@ -144,31 +254,88 @@ twpConfig.onReady(function() {
                 break
         }
 
-        selTextButton = shadowRoot.getElementById("selTextButton")
-        selTextTranslated = shadowRoot.getElementById("selTextTranslated")
+        eButtonTransSelText = shadowRoot.getElementById("eButtonTransSelText")
+        eDivResult = shadowRoot.getElementById("eDivResult")
+        eSelTextTrans = shadowRoot.getElementById("eSelTextTrans")
+        
+        const sGoogle = shadowRoot.getElementById("sGoogle")
+        const sYandex = shadowRoot.getElementById("sYandex")
+
+        sGoogle.onclick = () => {
+            currentPageTranslatorService = "google"
+            translateSelText(true)
+
+            sGoogle.classList.remove("selected")
+            sYandex.classList.remove("selected")
+
+            sGoogle.classList.add("selected")
+        }
+        sYandex.onclick = () => {
+            currentPageTranslatorService = "yandex"
+            translateSelText(true)
+
+            sGoogle.classList.remove("selected")
+            sYandex.classList.remove("selected")
+
+            sYandex.classList.add("selected")
+        }
+
+        const setTargetLanguage = shadowRoot.getElementById("setTargetLanguage")
+        setTargetLanguage.onclick = e => {
+            if (e.target.getAttribute("value")) {
+                const langCode = twpLang.checkLanguageCode(e.target.getAttribute("value"))
+                if (langCode) {
+                    currentTargetLanguage = langCode
+                    translateSelText(true)
+                }
+            }
+
+            shadowRoot.querySelectorAll("#setTargetLanguage li").forEach(li => {
+                li.classList.remove("selected")
+            })
+
+            e.target.classList.add("selected")
+        }
 
         document.body.appendChild(divElement)
 
         if (plataformInfo.isMobile.any) {
-            selTextButton.style.width = "30px"
-            selTextButton.style.height = "30px"
+            eButtonTransSelText.style.width = "30px"
+            eButtonTransSelText.style.height = "30px"
             document.addEventListener("touchstart", onTouchstart)
         }
 
-        selTextButton.addEventListener("click", onClick)
+        eButtonTransSelText.addEventListener("click", onClick)
         document.addEventListener("mousedown", onDown)
+
+        const targetLanguageButtons = shadowRoot.querySelectorAll("#setTargetLanguage li")
+
+        for (let i = 0; i < 3; i++) {
+            if (currentTargetLanguages[i] == currentTargetLanguage) {
+                targetLanguageButtons[i].classList.add("selected")
+            }
+            targetLanguageButtons[i].textContent = currentTargetLanguages[i]
+            targetLanguageButtons[i].setAttribute("value", currentTargetLanguages[i])
+            targetLanguageButtons[i].setAttribute("title", twpLang.codeToLanguage(currentTargetLanguages[i]))
+        }
+
+        if (currentPageTranslatorService === "yandex") {
+            sYandex.classList.add("selected")
+        } else {
+            sGoogle.classList.add("selected")
+        }
     }
 
     function destroy() {
         if (!divElement) return;
 
-        selTextButton.removeEventListener("click", onClick)
+        eButtonTransSelText.removeEventListener("click", onClick)
         document.removeEventListener("mousedown", onDown)
         if (plataformInfo.isMobile.any) {
             document.removeEventListener("touchstart", onTouchstart)
         }
         divElement.remove()
-        divElement = selTextButton = selTextTranslated = null
+        divElement = eButtonTransSelText = eDivResult = null
     }
 
     twpConfig.onChanged(function (name, newValue) {
@@ -177,7 +344,8 @@ twpConfig.onReady(function() {
                 currentPageTranslatorService = newValue
                 break
             case "targetLanguages":
-                currentTargetLanguage = newValue
+                currentTargetLanguage = newValue[0]
+                currentTargetLanguages = newValue
                 break
             case "neverTranslateSites":
                 translateThisSite = newValue.indexOf(location.hostname) === -1
@@ -209,47 +377,52 @@ twpConfig.onReady(function() {
     })
 
     let gSelectionInfo
+    let prevSelectionInfo
 
-    function translateSelText() {
-        if (gSelectionInfo) {
-            backgroundTranslateSingleText(currentPageTranslatorService, currentTargetLanguage, gSelectionInfo.text)
-            .then(result => {
-                if(!result) return
-                init()
-                const eTop = gSelectionInfo.bottom
-                const eLeft = gSelectionInfo.left
-
-                selTextTranslated.style.top = "0px"
-                selTextTranslated.style.left = "0px"
-                selTextTranslated.textContent = result
-                selTextTranslated.style.display = "block"
-    
-                const height = parseInt(selTextTranslated.offsetHeight)
-                let top = eTop + 5
-                top = Math.max(0, top)
-                top = Math.min(window.innerHeight - height, top)
-    
-                const width = parseInt(selTextTranslated.offsetWidth)
-                let left = parseInt(eLeft /*- width / 2*/)
-                left = Math.max(0, left)
-                left = Math.min(window.innerWidth - width, left)
-    
-                selTextTranslated.style.top = top + "px"
-                selTextTranslated.style.left = left + "px"
-            })
+    function translateSelText(usePrevSelectionInfo=false) {
+        if (!usePrevSelectionInfo && gSelectionInfo) {
+            prevSelectionInfo = gSelectionInfo
+        } else if (!(usePrevSelectionInfo && prevSelectionInfo)) {
+            return
         }
+
+        backgroundTranslateSingleText(currentPageTranslatorService, currentTargetLanguage, prevSelectionInfo.text)
+        .then(result => {
+            if(!result) return
+            init()
+            const eTop = prevSelectionInfo.bottom
+            const eLeft = prevSelectionInfo.left
+
+            eDivResult.style.top = "0px"
+            eDivResult.style.left = "0px"
+            eSelTextTrans.textContent = result
+            eDivResult.style.display = "block"
+
+            const height = parseInt(eDivResult.offsetHeight)
+            let top = eTop + 5
+            top = Math.max(0, top)
+            top = Math.min(window.innerHeight - height, top)
+
+            const width = parseInt(eDivResult.offsetWidth)
+            let left = parseInt(eLeft /*- width / 2*/)
+            left = Math.max(0, left)
+            left = Math.min(window.innerWidth - width, left)
+
+            eDivResult.style.top = top + "px"
+            eDivResult.style.left = left + "px"
+        })
     }
 
 
     function onClick(e) {
         translateSelText()
-        selTextButton.style.display = "none"
+        eButtonTransSelText.style.display = "none"
     }
 
     function onDown(e) {
         if (e.target != divElement) {
-            selTextTranslated.style.display = "none"
-            selTextButton.style.display = "none"
+            eDivResult.style.display = "none"
+            eButtonTransSelText.style.display = "none"
             destroy()
         }
     }
@@ -327,14 +500,14 @@ twpConfig.onReady(function() {
         ) {
             init()
             if (plataformInfo.isMobile.any) {
-                selTextButton.style.left = window.innerWidth - 45 + "px"
-                selTextButton.style.top = clientY + "px"
+                eButtonTransSelText.style.left = window.innerWidth - 45 + "px"
+                eButtonTransSelText.style.top = clientY + "px"
             } else {
-                selTextButton.style.left = clientX + 20 + "px"
-                selTextButton.style.top = clientY - 30 + "px"
+                eButtonTransSelText.style.left = clientX + 20 + "px"
+                eButtonTransSelText.style.top = clientY - 30 + "px"
             }
 
-            selTextButton.style.display = "block"
+            eButtonTransSelText.style.display = "block"
         }
     }
     
