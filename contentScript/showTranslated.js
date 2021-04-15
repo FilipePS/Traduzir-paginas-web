1a5377bbfef3c434f39de3c68507337630785b38
Filipe PS <filipe.panni@gmail.com>
2021-04-15 20:08:41 -0300

    Adicionado: arrastar popup de tradução de texto

--- contentScript/showTranslated.js
+++ contentScript/showTranslated.js
@@ -188,6 +188,45 @@ twpConfig.onReady(function () {
         clearTimeout(timeoutHandler)
     }
 
+    function dragElement(elmnt, elmnt2) {
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        if (elmnt2) {
+            elmnt2.addEventListener("mousedown", dragMouseDown);
+        } else {
+            elmnt.addEventListener("mousedown", dragMouseDown);
+        }
+      
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.addEventListener("mouseup", closeDragElement);
+            // call a function whenever the cursor moves:
+            document.addEventListener("mousemove", elementDrag);
+        }
+      
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        }
+      
+        function closeDragElement() {
+            // stop moving when mouse button is released:
+            document.removeEventListener("mouseup", closeDragElement);
+            document.removeEventListener("mousemove", elementDrag);
+        }
+    }
+
     showTranslated.enable = function () {
         showTranslated.disable()
     
@@ -267,7 +306,7 @@ twpConfig.onReady(function () {
             <div id="eDivResult">
                 <div id="eTextTranslated"></div>
                 <hr>
-                <div style="display: flex; justify-content: space-between; flex-direction:row;">
+                <div style="display: flex; justify-content: space-between; flex-direction:row;" id="drag">
                     <ul id="setTargetLanguage">
                         <li value="en" title="English">en</li>
                         <li value="es" title="Spanish">es</li>
@@ -329,6 +368,8 @@ twpConfig.onReady(function () {
                 </div>
             </div>
         `
+        
+        dragElement(shadowRoot.getElementById("eDivResult"), shadowRoot.getElementById("drag"))
 
         function enableDarkMode() {
             if (!shadowRoot.getElementById("darkModeElement")) {
