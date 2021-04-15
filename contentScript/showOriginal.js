c3163a79b2096c7ae5d55a99c1ab359055a1b08e
Filipe PS <filipe.panni@gmail.com>
2021-04-15 18:36:24 -0300

    corrigido formatação do texto ao mostrar original

--- contentScript/showOriginal.js
+++ contentScript/showOriginal.js
@@ -93,7 +93,7 @@ twpConfig.onReady(function () {
         if (plataformInfo.isMobile.any) return;
 
         if (node && nodesToShowOriginal.indexOf(node) === -1) {
-            nodesToShowOriginal.push({node: node, original: node.textContent})
+            nodesToShowOriginal.push({node: node, original: node.innerText})
             node.addEventListener("mouseenter", onMouseEnter)
             node.addEventListener("mouseout", onMouseOut)
         }
