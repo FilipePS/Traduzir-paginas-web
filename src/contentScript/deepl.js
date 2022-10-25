e878d61c80ebeb6420ffe68e7050e5e03bc061e7
FilipePS <filipe.panni@gmail.com>
2022-10-25 13:46:48 -0300

    refatorado deepl.js

--- src/contentScript/deepl.js
+++ src/contentScript/deepl.js
@@ -1,69 +1,102 @@
 "use strict";
 
-{
-    function translate(text, targetLanguage) {
-        return new Promise(resolve => {
-            const source_textarea = document.querySelector("textarea[dl-test=translator-source-input]")
-            const target_textarea = document.querySelector("textarea[dl-test=translator-target-input]")
+/*
+  When the DeepL tab is not activated, the timers will have a minimum delay of 1 second which delays the translation.
+*/
 
-            try {
-                targetLanguage = targetLanguage.split("-")[0]
-                if (target_textarea.lang.split("-")[0].toLowerCase() !== targetLanguage) {
-                    document.querySelector(`button[dl-test=translator-target-lang-btn]`).click()
-                    document.querySelector(`button[dl-test|=translator-lang-option-${targetLanguage}]`).click()
-                } else if (target_textarea.value && text === source_textarea.value) {
-                    resolve(target_textarea.value)
-                    return
-                }
-            } catch (e) {
-                console.error(e)
-            }
+void (function () {
+  /**
+   * Put the original text in the correct place, select the target language, wait and return the translation result.
+   * @param {string} text
+   * @param {string} targetLanguage
+   * @returns {Promise<string>} resultText
+   */
+  async function translate(text, targetLanguage) {
+    return await new Promise((resolve) => {
+      /** @type {HTMLTextAreaElement} */
+      const source_textarea = document.querySelector(
+        "textarea[dl-test=translator-source-input]"
+      );
+      /** @type {HTMLTextAreaElement} */
+      const target_textarea = document.querySelector(
+        "textarea[dl-test=translator-target-input]"
+      );
 
-            const event = new Event("change")
+      // select the target language
+      try {
+        targetLanguage = targetLanguage.split("-")[0];
+        if (
+          target_textarea.lang.split("-")[0].toLowerCase() !== targetLanguage
+        ) {
+          /** @type {HTMLButtonElement} */
+          const buttonOpenTargetLanguageSelector = document.querySelector(
+            `button[dl-test=translator-target-lang-btn]`
+          );
+          buttonOpenTargetLanguageSelector.click();
 
-            source_textarea.value = text
-            source_textarea.dispatchEvent(event)
-
-            const startTime = performance.now()
-
-            function checkresult(oldvalue) {
-                if ((performance.now() - startTime) > 2400 || (target_textarea.value && target_textarea.value !== oldvalue)) {
-                    resolve(target_textarea.value)
-                    return
-                }
-                setTimeout(checkresult, 100, oldvalue)
-            }
-            checkresult(target_textarea.value)
-        })
-    }
-
-    if (location.hash.startsWith("#!")) {
-        let [targetLanguage, text] = location.hash.split("!#")
-        location.hash = ""
-
-        targetLanguage = decodeURIComponent(targetLanguage.substring(2))
-        text = decodeURIComponent(text)
-
-        translate(text, targetLanguage || "en")
-            .then(result => {
-                console.log(result)
-                chrome.runtime.sendMessage({
-                    action: "DeepL_firstTranslationResult",
-                    result
-                })
-            })
-    }
-
-    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-        if (request.action === "translateTextWithDeepL") {
-            console.info(request)
-            translate(request.text, request.targetLanguage)
-                .then(result => {
-                    console.log(result)
-                    sendResponse(result)
-                })
+          /** @type {HTMLButtonElement} */
+          const buttonSelectedTargetLanguage = document.querySelector(
+            `button[dl-test|=translator-lang-option-${targetLanguage}]`
+          );
+          buttonSelectedTargetLanguage.click();
+        } else if (target_textarea.value && text === source_textarea.value) {
+          resolve(target_textarea.value);
+          return;
         }
+      } catch (e) {
+        console.error(e);
+      }
 
-        return true
-    })
-}
+      // set the source language, the translation will start immediately
+      source_textarea.value = text;
+      source_textarea.dispatchEvent(new Event("change"));
+
+      const startTime = performance.now();
+
+      /**
+       * wait for the translation to finish
+       * @param {string} oldvalue
+       * @returns
+       */
+      function checkresult(oldvalue) {
+        if (
+          performance.now() - startTime > 2400 ||
+          (target_textarea.value && target_textarea.value !== oldvalue)
+        ) {
+          return resolve(target_textarea.value);
+        }
+        setTimeout(checkresult, 100, oldvalue);
+      }
+      checkresult(target_textarea.value);
+    });
+  }
+
+  // get the sourceText and targetLanguage from the URL hash
+  if (location.hash.startsWith("#!")) {
+    let [targetLanguage, text] = location.hash.split("!#");
+    location.hash = "";
+
+    targetLanguage = decodeURIComponent(targetLanguage.substring(2));
+    text = decodeURIComponent(text);
+
+    translate(text, targetLanguage || "en").then((result) => {
+      console.info(result);
+      chrome.runtime.sendMessage({
+        action: "DeepL_firstTranslationResult",
+        result,
+      });
+    });
+  }
+
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "translateTextWithDeepL") {
+      console.info(request);
+      translate(request.text, request.targetLanguage).then((result) => {
+        console.info(result);
+        sendResponse(result);
+      });
+    }
+
+    return true;
+  });
+})();
