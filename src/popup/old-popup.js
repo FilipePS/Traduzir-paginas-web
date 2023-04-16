6ec2a61e4e34327b3f2dca4841e54f1118ff596e
FilipePS <filipe.panni@gmail.com>
2023-04-16 18:34:12 -0300

    Corrigido: link do google tradutor - #553

--- src/popup/old-popup.js
+++ src/popup/old-popup.js
@@ -628,9 +628,7 @@ twpConfig.onReady(function () {
                 } else {
                   // google
                   chrome.tabs.create({
-                    url: `https://translate.google.${
-                      "zh-cn" == navigator.language.toLowerCase() ? "cn" : "com"
-                    }/translate?tl=${twpConfig.get(
+                    url: `https://translate.google.com/translate?tl=${twpConfig.get(
                       "targetLanguage"
                     )}&u=${encodeURIComponent(tabs[0].url)}`,
                   });
