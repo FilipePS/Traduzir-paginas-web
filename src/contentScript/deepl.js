22c14b1421859a6bb26e599c71faf06a5062626a
FilipePS <filipe.panni@gmail.com>
2024-02-15 15:49:17 -0300

    Adicionado mensagem informando razão da aba do deepl abrir

--- src/contentScript/deepl.js
+++ src/contentScript/deepl.js
@@ -46,8 +46,63 @@ void (function () {
     });
   }
 
+  function injectInformation() {
+    if (document.getElementById("twp-info")) return;
+    const style = document.createElement("style");
+    style.textContent = `
+    /* TWP - Translate Web Pages */
+    #twp-info button {
+      color: black;
+      background-color: white;
+      border: 1px solid black;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+      border-radius: 10px
+      cursor: pointer;
+      padding: 5px 10px;
+      border-radius: 10px;
+      transition: transform 0.1s;
+    }
+    #twp-info button:hover {
+      transform: scale(1.1);
+    }
+    #twp-info p {
+      color: white;
+    }
+    `;
+    document.head.appendChild(style);
+
+    const info = document.createElement("div");
+    info.setAttribute("id", "twp-info");
+    info.style.cssText = `
+      width: 100%;
+      padding: 10px;
+      text-align: center;
+      background: rgb(37, 108, 219);
+      box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.5);
+      padding: 7px;
+    `;
+    info.innerHTML = `
+      <p style="font-size: 20px; font-weight: bold;">TWP - Translate Web Pages</p>
+      <p data-i18n="msgDeepLTabReasoOpened">This tab opened because you clicked to translate selected text using DeepL.</p>
+      <button data-i18n="msgDontShowAgain">Don't show again</button>
+    `;
+    document.body.insertBefore(info, document.body.firstChild);
+
+    info.querySelector("button").addEventListener("click", () => {
+      twpConfig.set("textTranslatorService", "google");
+      setTimeout(() => window.close(), 500);
+    });
+
+    twpConfig.onReady(async () => {
+      await twpI18n.updateUiMessages();
+      twpI18n.translateDocument(info);
+    });
+  }
+
   // get the sourceText and targetLanguage from the URL hash
   if (location.hash.startsWith("#!")) {
+    injectInformation();
+
     let [targetLanguage, text] = location.hash.split("!#");
     location.hash = "";
 
